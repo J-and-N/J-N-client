9a66aee063629ffec3e18b20712587be6f9dc697
Jeongmin <srm0609@naver.com>
2023-01-02 20:35:23 +0900

    record

--- jn-client/src/components/Records/index.tsx
+++ jn-client/src/components/Records/index.tsx
@@ -2,18 +2,68 @@ import * as S from "./style";
 import TemperatureRe from "../common/TemperatureRe";
 import HumidityRe from "../common/HumidityRe";
 import Header from "../Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getAvgTemperature, getAvgHumidity } from "../../Api/state";
+import { getPageTemperature, getPageHumidity } from "../../Api/record";
+import { AvgType } from "../../types/graph";
+import { tempPageType, humPageType } from "../../types/record";
+import dateFillter from "../../Util/lib/dateFillter";
+import ArrowLeft from "public/images/ArrowLeft.svg";
+import ArrowRight from "public/images/ArrowRight.svg";
 
 const Records = () => {
-  const TempData = [
-    { num: "18866", tem: "26", date: "2022-10-27 22:24:32" },
-    { num: "18867", tem: "26", date: "2022-10-27 22:24:32" },
-    { num: "18868", tem: "26", date: "2022-10-27 22:24:32" },
-    { num: "18869", tem: "26", date: "2022-10-27 22:24:32" },
-  ];
-  const PageD = [1, 2, 3, 4, 5];
+  const [tempAvg, setTempAvg] = useState<AvgType>();
+  const [humAvg, setHumAvg] = useState<AvgType>();
+  const [pageTemp, setPageTemp] = useState<tempPageType[]>([]);
+  const [pageHum, setPageHum] = useState<humPageType[]>([]);
+  const [pageNum, setPageNum] = useState(1);
 
   const [TextState, setTextState] = useState(true);
+  // const [sever, setSever] = useState<severType>();
+
+  const prevPageClick = async () => {
+    setPageNum(pageNum - 1);
+    const sever = localStorage.getItem("Servername");
+
+    return TextState
+      ? getPageTemperature(pageNum - 1, sever).then((res) => {
+          setPageTemp(res?.data);
+        })
+      : getPageHumidity(pageNum - 1, sever).then((res) => {
+          setPageHum(res?.data);
+        });
+  };
+  const nextPageClick = async () => {
+    setPageNum(pageNum + 1);
+    const sever = localStorage.getItem("Servername");
+
+    return TextState
+      ? getPageTemperature(pageNum + 1, sever).then((res) => {
+          setPageTemp(res?.data);
+        })
+      : getPageHumidity(pageNum + 1, sever).then((res) => {
+          setPageHum(res?.data);
+        });
+  };
+
+  useEffect(() => {
+    const sever = localStorage.getItem("Servername");
+    // setSever(sever);
+    getAvgTemperature(sever).then((res) => {
+      setTempAvg(res?.data);
+    });
+    getAvgHumidity(sever).then((res) => {
+      setHumAvg(res?.data);
+    });
+    getPageTemperature(1, sever).then((res) => {
+      console.log(res?.data);
+      setPageTemp(res?.data);
+    });
+    getPageHumidity(1, sever).then((res) => {
+      console.log(res?.data);
+      setPageHum(res?.data);
+    });
+  }, []);
 
   const chgTempState = () => {
     setTextState(true);
@@ -38,52 +88,94 @@ const Records = () => {
         {TextState ? (
           <>
             <S.ContentWrap>
-              <TemperatureRe Date={"Now"} />
-              <TemperatureRe Date={"Day"} />
-              <TemperatureRe Date={"Week"} />
+              <TemperatureRe Date={"Now"} TEMP={tempAvg?.now} />
+              <TemperatureRe Date={"Day"} TEMP={tempAvg?.day} />
+              <TemperatureRe Date={"Week"} TEMP={tempAvg?.week} />
             </S.ContentWrap>
             <S.DListWrap>
               <S.DBack>
-                {TempData.length !== 0 ? (
-                  TempData.map((e) => (
-                    <S.DList key={e.num}>
-                      <S.DNum>{e.num}</S.DNum>
+                <S.Class>
+                  <p>ID</p>
+                  <div>
+                    <p>TEMP</p>
+                    <p>Date</p>
+                  </div>
+                </S.Class>
+                {pageTemp.length !== 0 ? (
+                  pageTemp.map((e) => (
+                    <S.DList key={e.id}>
+                      <S.DNum>{e.id}</S.DNum>
                       <S.Wr>
-                        <S.DTem>{e.tem}°C</S.DTem>
-                        <S.DDAte>{e.date}</S.DDAte>
+                        <S.DTem>{e.TEMP}°C</S.DTem>
+                        <S.DDAte>{dateFillter(e.TIME)}</S.DDAte>
                       </S.Wr>
                     </S.DList>
                   ))
                 ) : (
                   <p>기록이 없습니다.</p>
                 )}
-                <S.PageNum>{PageD.map((e) => e)}</S.PageNum>
+                <S.PageNum>
+                  {pageNum === 1 ? (
+                    <>
+                      <S.Empty></S.Empty>
+                    </>
+                  ) : (
+                    <div onClick={prevPageClick}>
+                      <ArrowLeft />
+                    </div>
+                  )}
+                  <label>{pageNum}</label>
+                  <div onClick={nextPageClick}>
+                    <ArrowRight />
+                  </div>
+                </S.PageNum>{" "}
               </S.DBack>
             </S.DListWrap>
           </>
         ) : (
           <>
             <S.ContentWrap>
-              <HumidityRe Date={"Now"} />
-              <HumidityRe Date={"Day"} />
-              <HumidityRe Date={"Week"} />
+              <HumidityRe Date={"Now"} HUM={humAvg?.now} />
+              <HumidityRe Date={"Day"} HUM={humAvg?.day} />
+              <HumidityRe Date={"Week"} HUM={humAvg?.week} />
             </S.ContentWrap>
             <S.DListWrap>
               <S.DBack>
-                {TempData.length !== 0 ? (
-                  TempData.map((e) => (
-                    <S.DList key={e.num}>
-                      <S.DNum>{e.num}</S.DNum>
+                <S.Class>
+                  <p>ID</p>
+                  <div>
+                    <p>HUM</p>
+                    <p>Date</p>
+                  </div>
+                </S.Class>
+                {pageHum.length !== 0 ? (
+                  pageHum.map((e) => (
+                    <S.DList key={e.id}>
+                      <S.DNum>{e.id}</S.DNum>
                       <S.Wr>
-                        <S.DTem>{e.tem}°C</S.DTem>
-                        <S.DDAte>{e.date}</S.DDAte>
+                        <S.DTem>{e.DEGREE}%</S.DTem>
+                        <S.DDAte>{dateFillter(e.TIME)}</S.DDAte>
                       </S.Wr>
                     </S.DList>
                   ))
                 ) : (
                   <p>기록이 없습니다.</p>
                 )}
-                <S.PageNum>{PageD.map((e) => e)}</S.PageNum>
+                <S.PageNum>
+                  {pageNum === 1 ? (
+                    <>
+                      <S.Empty></S.Empty>
+                    </>
+                  ) : (
+                    <div onClick={prevPageClick}>
+                      <ArrowLeft />
+                    </div>
+                  )}
+                  <label>{pageNum}</label>
+                  <div onClick={nextPageClick}>
+                    <ArrowRight />
+                  </div>
+                </S.PageNum>
               </S.DBack>
             </S.DListWrap>
           </>
--- jn-client/src/components/Records/style.ts
+++ jn-client/src/components/Records/style.ts
@@ -52,22 +52,69 @@ export const DBack = styled.div`
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
 `;
+export const Class = styled.div`
+  width: 100%;
+  height: 30px;
+  font-size: 25px;
+  margin-top: -10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  opacity: 0.5;
+  p:nth-of-type(1) {
+    width: 5%;
+    display: flex;
+    justify-content: center;
+  }
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 30%;
+    p:nth-of-type(1) {
+      width: 10%;
+      display: flex;
+      justify-content: center;
+    }
+    p:nth-of-type(2) {
+      width: 80%;
+      display: flex;
+      justify-content: center;
+    }
+  }
+`;
 export const DList = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   font-size: 30px;
 `;
 export const Wr = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   width: 30%;
 `;
-export const DNum = styled.div``;
-export const DTem = styled.div``;
+export const DNum = styled.div`
+  margin-top: 5px;
+`;
+export const DTem = styled.div`
+  margin-top: 5px;
+`;
 export const DDAte = styled.div``;
 
 export const PageNum = styled.div`
   display: flex;
   justify-content: center;
-  font-size: 25px;
+  font-size: 29px;
+  div {
+    display: flex;
+    opacity: 0.5;
+    margin-left: 20px;
+    margin-right: 20px;
+  }
+`;
+export const Empty = styled.div`
+  width: 30px;
+  height: 30px;
 `;
